a291b00090ef1d28f378afaed57c3b278a7762a1
WrathOfZombies <bhargav.krishna@outlook.com>
2018-06-13 16:15:17 -0700

    Base icon component

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export Image from './components/Image'
+export Layout from './components/Layout'
+export List from './components/List'
+export Provider from './components/Provider'
+export ProviderConsumer from './components/Provider/ProviderConsumer'
+export Icon from './components/Icon'
