ab554b77604a30f9ce8a0285d4ba6b4dd0aca27a
Bhargav Krishna <bhkrish@microsoft.com>
2018-06-18 12:24:37 -0700

    Finalizing Icon component port

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { createComponent, getElementType, getUnhandledProps } from '../../lib'
+import cx from 'classnames'
+import _ from 'lodash'
+import {
+  createComponent,
+  getElementType,
+  getUnhandledProps,
+  SUI,
+  customPropTypes,
+  useKeyOnly,
+  useValueAndKey,
+} from '../../lib'
 
 class Icon extends React.Component {
   static propTypes = {
     /** An element type to render as (string or function). */
-    // as: customPropTypes.as,
+    as: customPropTypes.as,
 
     /** Formatted to appear bordered. */
     bordered: PropTypes.bool,
@@ -17,7 +27,7 @@ class Icon extends React.Component {
     className: PropTypes.string,
 
     /** Color of the icon. */
-    // color: PropTypes.oneOf(SUI.COLORS),
+    color: PropTypes.oneOf(SUI.COLORS),
 
     /** Icons can display a smaller corner icon. */
     corner: PropTypes.bool,
@@ -41,20 +51,20 @@ class Icon extends React.Component {
     loading: PropTypes.bool,
 
     /** Name of the icon. */
-    // name: customPropTypes.suggest(SUI.ALL_ICONS_IN_ALL_CONTEXTS),
+    name: customPropTypes.suggest(SUI.ALL_ICONS_IN_ALL_CONTEXTS),
 
     /** Icon can rotated. */
     rotated: PropTypes.oneOf(['clockwise', 'counterclockwise']),
 
     /** Size of the icon. */
-    // size: PropTypes.oneOf(_.without(SUI.SIZES, 'medium')),
+    size: PropTypes.oneOf(_.without(SUI.SIZES, 'medium')),
 
     /** Icon can have an aria label. */
     'aria-label': PropTypes.string,
   }
 
   static defaultProps = {
-    // as: 'i',
+    as: 'i',
   }
 
   getIconAriaOptions() {
@@ -69,45 +79,45 @@ class Icon extends React.Component {
   }
 
   render() {
-    // const {
-    //   bordered,
-    //   circular,
-    //   className,
-    //   color,
-    //   corner,
-    //   disabled,
-    //   fitted,
-    //   flipped,
-    //   inverted,
-    //   link,
-    //   loading,
-    //   name,
-    //   rotated,
-    //   size,
-    // } = this.props
+    const {
+      bordered,
+      circular,
+      className,
+      color,
+      corner,
+      disabled,
+      fitted,
+      flipped,
+      inverted,
+      link,
+      loading,
+      name,
+      rotated,
+      size,
+    } = this.props
 
-    // const classes = cx(
-    //   color,
-    //   name,
-    //   size,
-    //   useKeyOnly(bordered, 'bordered'),
-    //   useKeyOnly(circular, 'circular'),
-    //   useKeyOnly(corner, 'corner'),
-    //   useKeyOnly(disabled, 'disabled'),
-    //   useKeyOnly(fitted, 'fitted'),
-    //   useKeyOnly(inverted, 'inverted'),
-    //   useKeyOnly(link, 'link'),
-    //   useKeyOnly(loading, 'loading'),
-    //   useValueAndKey(flipped, 'flipped'),
-    //   useValueAndKey(rotated, 'rotated'),
-    //   'icon',
-    //   className,
-    // )
+    const classes = cx(
+      color,
+      name,
+      size,
+      useKeyOnly(bordered, 'bordered'),
+      useKeyOnly(circular, 'circular'),
+      useKeyOnly(corner, 'corner'),
+      useKeyOnly(disabled, 'disabled'),
+      useKeyOnly(fitted, 'fitted'),
+      useKeyOnly(inverted, 'inverted'),
+      useKeyOnly(link, 'link'),
+      useKeyOnly(loading, 'loading'),
+      useValueAndKey(flipped, 'flipped'),
+      useValueAndKey(rotated, 'rotated'),
+      'icon',
+      className,
+    )
+
     const rest = getUnhandledProps(Icon, this.props)
     const ElementType = getElementType(Icon, this.props)
     const ariaOptions = this.getIconAriaOptions()
-
-    return <ElementType {...rest} {...ariaOptions} />
+    return <ElementType {...rest} {...ariaOptions} className={classes} />
   }
 }
 
---
+++ src/lib/classNameBuilders.js
@@ -0,0 +1,21 @@
+/**
+ * Props where only the prop key is used in the className.
+ * @param {*} val A props value
+ * @param {string} key A props key
+ *
+ * @example
+ * <Label tag />
+ * <div class="ui tag label"></div>
+ */
+export const useKeyOnly = (val, key) => val && key
+
+/**
+ * Props that require both a key and value to create a className.
+ * @param {*} val A props value
+ * @param {string} key A props key
+ *
+ * @example
+ * <Label corner='left' />
+ * <div class="ui left corner label"></div>
+ */
+export const useValueAndKey = (val, key) => val && val !== true && `${val} ${key}`
