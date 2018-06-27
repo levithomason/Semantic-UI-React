5be886bad4aa16877c2f343e2105c595bae6589e
manajdov <manajdov@microsoft.com>
2018-06-27 11:01:31 +0200

    -rounded up the padding values

--- src/components/Label/labelRules.ts
+++ src/components/Label/labelRules.ts
@@ -2,7 +2,7 @@ import { rem } from '../../lib'
 
 export default ({ active, circular, variables }) => ({
   root: {
-    padding: `${rem(0.5833)} ${rem(0.833)}`,
+    padding: `${rem(0.6)} ${rem(0.8)}`,
     fontWeight: '500',
     backgroundColor: 'rgb(232, 232, 232)',
     color: 'rgba(0, 0, 0, 0.6)',
