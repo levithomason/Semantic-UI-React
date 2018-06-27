04a5393758c6801c660cc60f8debfd75ab8ad000
Alexandru Buliga <albuliga@microsoft.com>
2018-06-27 12:36:27 +0200

    feat(Button): add first two button variations

--- src/components/Button/buttonVariables.ts
+++ src/components/Button/buttonVariables.ts
@@ -1,10 +1,27 @@
 import { rem } from '../../lib'
 
-export default () => {
-  const vars: any = {}
+export interface IButtonVariables {
+  circularRadius: string
+  circularWidth: string
+  typePrimaryColor: string
+  typePrimaryBackgroundColor: string
+  typePrimaryBorderColor: string
+  typeSecondaryColor: string
+  typeSecondaryBackgroundColor: string
+  typeSecondaryBorderColor: string
+}
 
-  vars.circularRadius = rem(999)
-  vars.circularWidth = rem(4.2)
+export default (siteVars: any): IButtonVariables => {
+  const { white, black, btnPrimaryColor, btnSecondaryColor, btnSecondaryBorderColor } = siteVars
 
-  return vars
+  return {
+    circularRadius: rem(10),
+    circularWidth: '32px', // TODO-Bugaa92: convert to using rem when it allows input as px
+    typePrimaryColor: white,
+    typePrimaryBackgroundColor: btnPrimaryColor,
+    typePrimaryBorderColor: 'transparent',
+    typeSecondaryColor: black,
+    typeSecondaryBackgroundColor: btnSecondaryColor,
+    typeSecondaryBorderColor: btnSecondaryBorderColor,
+  }
 }
