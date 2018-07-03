ae4a99929724ed66eaa384b22bdd339124dd8e22
manajdov <manajdov@microsoft.com>
2018-07-03 11:07:01 +0200

    -resolving comments on the PR

--- docs/src/components/ComponentDoc/ComponentDocTag.tsx
+++ docs/src/components/ComponentDoc/ComponentDocTag.tsx
@@ -1,32 +1,33 @@
 import React, { Component } from 'react'
-import { Header } from 'semantic-ui-react'
+import _ from 'lodash'
+import { Header, Message } from 'semantic-ui-react'
 
 const headerStyle = {
   whiteSpace: 'pre-line',
 }
 
 class ComponentDocTag extends Component<any, any> {
-  getTagDescription = (info, tagParam) => {
-    const tags = (info.docblock && info.docblock.tags) || []
-    let accessibilityDescription = null
-    for (const tag of tags) {
-      if (tag.title === tagParam) {
-        accessibilityDescription = tag.description
-      }
-    }
-    return accessibilityDescription
+  getTagDescription = (forTag, fromInfo) => {
+    const tags = (fromInfo.docblock && fromInfo.docblock.tags) || []
+    return _.result(_.find(tags, 'title', forTag), 'description')
   }
 
   render() {
     const { info, tag, title } = this.props
-    const description = this.getTagDescription(info, tag)
+    const description = this.getTagDescription(tag, info) || (
+      <Message
+        error
+        content="No accessibility description found for this component. Please explain the
+        accessibility concerns of the component by adding the @accessibility tag to the component's docblock."
+        compact={true}
+      />
+    )
 
     return (
-      <React.Fragment>
-        {description && (
-          <Header as="h2" content={title} subheader={description} style={headerStyle} />
-        )}
-      </React.Fragment>
+      <Header as="h2" style={headerStyle}>
+        <Header.Content>{title}</Header.Content>
+        <Header.Subheader>{description}</Header.Subheader>
+      </Header>
     )
   }
 }
