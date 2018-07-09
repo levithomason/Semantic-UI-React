c82d9be9ff989549a8a68008c3a21ce7f0d47091
manajdov <manajdov@microsoft.com>
2018-07-09 17:03:06 +0200

    -fixes

--- docs/src/examples/components/Accordion/Types/AccordionExclusiveExample.tsx
+++ docs/src/examples/components/Accordion/Types/AccordionExclusiveExample.tsx
@@ -3,17 +3,17 @@ import { Accordion } from 'stardust'
 
 const panels = [
   {
-    key: 'one',
+    key: '1',
     title: 'One',
     content: '2 3 4',
   },
   {
-    key: 'five',
+    key: '5',
     title: 'Five',
     content: '6 7 8 9',
   },
   {
-    key: 'ten',
+    key: '10',
     title: "What's next?",
     content: '10',
   },
