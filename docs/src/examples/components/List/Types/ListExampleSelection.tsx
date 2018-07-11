04b8488be1b72bf8ad5051c71455e115da7abbbc
Levi Thomason <me@levithomason.com>
2018-07-10 21:46:52 -0700

    remove pxToREM from examples and cleanup strings

--- docs/src/examples/components/List/Types/ListExampleSelection.tsx
+++ docs/src/examples/components/List/Types/ListExampleSelection.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { List } from 'stardust'
-import { pxToRem } from 'src/lib/fontSizeUtility'
 
-const imgStyle = { display: 'block', width: pxToRem(28), borderRadius: pxToRem(9999) }
+const imgStyle = { display: 'block', width: '2rem', borderRadius: '999rem' }
 
 const ListExampleSelection = ({ knobs }) => (
   <List
@@ -13,15 +12,14 @@ const ListExampleSelection = ({ knobs }) => (
         media: <img src="/public/images/avatar/small/matt.jpg" style={imgStyle} />,
         header: 'Irving Kuhic',
         headerMedia: '7:26:56 AM',
-        content:
-          'If we program the sensor, we can get to the SAS alarm through the haptic SQL card!',
+        content: 'Program the sensor to the SAS alarm through the haptic SQL card!',
       },
       {
         key: 'skyler',
         media: <img src="/public/images/avatar/small/steve.jpg" style={imgStyle} />,
         header: 'Skyler Parks',
         headerMedia: '11:30:17 PM',
-        content: 'Use the online FTP application, then you can input the multi-byte application!',
+        content: 'Use the online FTP application to input the multi-byte application!',
       },
       {
         key: 'dante',
--- docs/src/examples/components/List/Variations/ListExampleTruncate.tsx
+++ docs/src/examples/components/List/Variations/ListExampleTruncate.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { List } from 'stardust'
-import { pxToRem } from 'src/lib/fontSizeUtility'
 
-const imgStyle = { display: 'block', width: pxToRem(28), borderRadius: pxToRem(9999) }
+const imgStyle = { display: 'block', width: '2rem', borderRadius: '999rem' }
 
 const ListExample = ({ knobs }) => (
   <div style={{ width: knobs.width }}>
@@ -15,15 +14,14 @@ const ListExample = ({ knobs }) => (
           media: <img src="/public/images/avatar/small/matt.jpg" style={imgStyle} />,
           header: 'Irving Kuhic - Super long title here',
           headerMedia: '7:26:56 AM',
-          content:
-            'If we program the sensor, we can get to the SAS alarm through the haptic SQL card!',
+          content: 'Program the sensor to the SAS alarm through the haptic SQL card!',
           contentMedia: '!!',
         },
         {
           media: <img src="/public/images/avatar/small/steve.jpg" style={imgStyle} />,
           header: 'Skyler Parks - Super long title here',
           headerMedia: '11:30:17 PM',
-          content: 'Use the online FTP application, then you can input the multi-byte application!',
+          content: 'Use the online FTP application to input the multi-byte application!',
           contentMedia: '!!',
         },
         {
