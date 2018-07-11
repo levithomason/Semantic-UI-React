cd3a15f327b1443312c0994db1507c3a7e32b05f
manajdov <manajdov@microsoft.com>
2018-07-11 10:04:03 +0200

    -removed property tests that will be covered by visual tests

--- test/specs/components/Divider/Divider-test.tsx
+++ test/specs/components/Divider/Divider-test.tsx
@@ -2,35 +2,7 @@ import React from 'react'
 
 import { isConformant } from '../../commonTests'
 import Divider from 'src/components/Divider/Divider'
-import { getTestingRenderedComponent } from 'test/utils'
 
 describe('Divider', () => {
   isConformant(Divider)
-
-  describe('type', () => {
-    const typeProp = 'type'
-
-    it('is not set by default', () => {
-      const dividerType = getTestingRenderedComponent(Divider, <Divider />).prop(typeProp)
-      expect(dividerType).toBeUndefined()
-    })
-
-    it('can be set to primary', () => {
-      const type = 'primary'
-      const dividerType = getTestingRenderedComponent(Divider, <Divider type={type} />).prop(
-        typeProp,
-      )
-
-      expect(dividerType).toEqual(type)
-    })
-
-    it('can be set to secondary', () => {
-      const type = 'secondary'
-      const dividerType = getTestingRenderedComponent(Divider, <Divider type={type} />).prop(
-        typeProp,
-      )
-
-      expect(dividerType).toEqual(type)
-    })
-  })
 })
