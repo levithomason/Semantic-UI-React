c2e66ac6d8e3fde3ed5750ecf51d4991c1d3d594
Alexandru Buliga <bugaaa92@gmail.com>
2018-07-16 18:32:08 +0200

    feat(button): added icon support

---
+++ docs/src/examples/components/Button/Types/ButtonExampleLabeledIcon.shorthand.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'stardust'
+
+const ButtonExampleLabeledIcon = () => (
+  <div>
+    <Button type="primary" icon="book" content="Click me left" iconPosition="left" />
+    <Button type="secondary" icon="coffee" content="Click me right" iconPosition="right" />
+  </div>
+)
+
+export default ButtonExampleLabeledIcon
